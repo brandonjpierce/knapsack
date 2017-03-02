419c0a4784b06aa69ac7038158072714d7e2cc37
Brandon Pierce <brandonpierce91@gmail.com>
2017-03-02 09:35:05 -0600

    Add no emit errors plugin and add to common preset

--- packages/knapsack-preset-common/index.js
+++ packages/knapsack-preset-common/index.js
@@ -1,11 +1,14 @@
 const merge = require('webpack-merge');
 const defineEnv = require('knapsack-plugin-define-env');
 const nodePackages = require('knapsack-plugin-node-packages');
+const noEmitErrors = require('knapsack-plugin-no-emit-errors');
 
 module.exports = merge.smart(
   defineEnv,
   nodePackages,
+  noEmitErrors,
   {
+    target: 'web',
     resolve: {
       extensions: ['.js', '.json', '.jsx'],
     },
