fbc35ab03ea26ccd49d477c3231ed0413a43dc30
Brandon Pierce <brandonpierce91@gmail.com>
2017-03-02 09:35:57 -0600

    Add notes to no emit error plugin

--- packages/knapsack-plugin-no-emit-errors/index.js
+++ packages/knapsack-plugin-no-emit-errors/index.js
@@ -2,6 +2,8 @@ const webpack = require('webpack');
 
 module.exports = {
   plugins: [
+    // This plugin does not swallow errors. Instead, it just prevents
+    // Webpack from printing out compile time stats to the console.
     new webpack.NoEmitOnErrorsPlugin(),
   ],
 };
