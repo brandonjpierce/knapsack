50ae0faf8baaf4a1413be1c46281628f3f41f1bb
Brandon Pierce <brandonpierce91@gmail.com>
2017-03-03 23:37:59 -0600

    Only provide NODE_ENV to define-env plugin

--- packages/knapsack-plugin-define-env/index.js
+++ packages/knapsack-plugin-define-env/index.js
@@ -1,14 +1,9 @@
-const assign = require('lodash/assign');
 const webpack = require('webpack');
 
-const defaults = {
-  NODE_ENV: 'development'
-};
-
-module.exports = opts => () => ({
+module.exports = () => () => ({
   plugins: [
     // Makes some environment variables available to the JS code, for example:
     // if (process.env.NODE_ENV === 'development') { ... }.
-    new webpack.EnvironmentPlugin(assign({}, opts, defaults))
+    new webpack.EnvironmentPlugin({NODE_ENV: 'development'})
   ]
 });
