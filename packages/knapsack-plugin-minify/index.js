96148049b17a40a6384a3e9e694d96e5d7b02f73
Brandon Pierce <brandonpierce91@gmail.com>
2017-03-05 17:26:29 -0600

    Add back in loader options optmization

--- packages/knapsack-plugin-minify/index.js
+++ packages/knapsack-plugin-minify/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-dynamic-require */
 
 const assign = require('lodash/assign');
+const webpack = require('webpack');
 const optimize = require('./optimize');
 
 const defaults = {
@@ -11,10 +12,16 @@ const defaults = {
 
 module.exports = opts => () => {
   const config = assign({}, defaults, opts);
-
   // TODO ensure either babili or uglify is passed for opts.minifier
   const minifier = require(`./${config.minifier}`);
-  const plugins = [minifier(config.options)];
+
+  const plugins = [
+    minifier(config.options),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    })
+  ];
 
   if (config.optimize) {
     plugins.push(optimize);
